4ce9f687cbd09b05fdde91f4fcb0e4571bcb6119
yoonsang Lee <highp0912@pm.me>
2022-04-05 00:28:24 +0900

    tidy: Button usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import { useContext } from "react";
-import {
-  GET_getCommentById,
-  GET_getPostById,
-  GET_getProfileByName,
-} from "./api/mock/mock.service";
-import "./App.css";
-import { loginAsync, logoutAsync } from "./fb/auth";
-import { AuthCtx } from "./fb/auth/auth.provider";
+import { useContext } from 'react';
+import Button from './component/layout/Button';
+// import {
+//   GET_getCommentById,
+//   GET_getPostById,
+//   GET_getProfileByName,
+// } from "./api/mock/mock.service";
+import Auth from './fb/auth';
+import { AuthCtx } from './fb/auth/auth.provider';
 
-GET_getPostById(1).then(console.log);
-GET_getCommentById(1).then(console.log);
-GET_getProfileByName("typicode").then(console.log);
+// GET_getPostById(1).then(console.log);
+// GET_getCommentById(1).then(console.log);
+// GET_getProfileByName("typicode").then(console.log);
 
 function App() {
-  const { user } = useContext(AuthCtx);
-  const loginButton = <button onClick={loginAsync}>Login</button>;
-  const logoutButton = <button onClick={logoutAsync}>Logout</button>;
-  return <div className="App">{!user ? loginButton : logoutButton}</div>;
+  const { authInfo, setAuthInfo } = useContext(AuthCtx);
+
+  const AuthButtonJSX = authInfo ? (
+    <Button onClick={() => Auth.signOutAsync(setAuthInfo)} text='Logout' />
+  ) : (
+    <Button onClick={() => Auth.signInAsync(setAuthInfo)} text='Login' />
+  );
+
+  return <div style={{ marginLeft: '5rem' }}>{AuthButtonJSX}</div>;
 }
 
 export default App;
