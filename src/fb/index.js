d38a99b2e9d15140a2f5fcf91b89c2b0c8c8b8e3
yoonsang Lee <highp0912@pm.me>
2022-04-04 23:55:37 +0900

    add: init firebase App with envMap

--- src/fb/index.js
+++ src/fb/index.js
@@ -1,15 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { envMap } from "../env";
+import { initializeApp } from 'firebase/app';
+import { envMap } from '../env';
 
 const firebaseConfig = {
-  apiKey: envMap.get("apiKey"),
-  authDomain: envMap.get("authDomain"),
-  projectId: envMap.get("projectId"),
+  apiKey: envMap.get('apiKey'),
+  authDomain: envMap.get('authDomain'),
+  projectId: envMap.get('projectId'),
   // storageBucket: envMap.get('storageBucket'),
   // messagingSenderId: envMap.get('messagingSenderId'),
-  appId: envMap.get("appId"),
+  appId: envMap.get('appId')
   //   measurementId: envMap.get("measurementId"),
 };
 
 export const app = initializeApp(firebaseConfig);
-console.log(app);
