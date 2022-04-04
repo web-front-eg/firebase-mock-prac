07f4a36aaa252f981a008536ad5fd80b2a610441
yoonsang Lee <highp0912@pm.me>
2022-04-05 01:25:47 +0900

    add: json-server + routes & rest client

--- src/api/mock/mock.service.js
+++ src/api/mock/mock.service.js
@@ -1,24 +1,24 @@
-import Axi from '../axios';
+// import Axi from '../axios';
 
-class MockService {
-  async GET_getPostById(id) {
-    const response = await Axi.getMethods().get(`posts/${id}`);
-    return response.data;
-  }
+// class MockService {
+//   async GET_getPostById(id) {
+//     const response = await Axi.getMethods().get(`posts/${id}`);
+//     return response.data;
+//   }
 
-  async GET_getCommentById(id) {
-    const response = await Axi.getMethods().get(`comments/${id}`);
-    return response.data;
-  }
+//   async GET_getCommentById(id) {
+//     const response = await Axi.getMethods().get(`comments/${id}`);
+//     return response.data;
+//   }
 
-  async GET_getProfileByName(name) {
-    if (typeof name !== 'string') {
-      throw new Error('name must be type of string');
-    }
+//   async GET_getProfileByName(name) {
+//     if (typeof name !== 'string') {
+//       throw new Error('name must be type of string');
+//     }
 
-    const response = await Axi.getMethods().get(`profile`);
-    return response.data;
-  }
-}
+//     const response = await Axi.getMethods().get(`profile`);
+//     return response.data;
+//   }
+// }
 
-export default new MockService();
+// export default new MockService();
