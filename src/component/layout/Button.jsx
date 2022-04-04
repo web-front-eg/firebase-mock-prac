e70f5c873b209f41a31733b326ea9e7897bee3ab
yoonsang Lee <highp0912@pm.me>
2022-04-05 00:10:14 +0900

    add: component/button

---
+++ src/component/layout/Button.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+
+const FONT_WEIGHT = isBold => (isBold ? 700 : 300);
+const defaultOnClickFn = () => console.log('button is clicked!');
+
+function Button({
+  onClick = defaultOnClickFn,
+  text = 'button',
+  isBold = false,
+  additionalStyle = {}
+}) {
+  return (
+    <button
+      onClick={onClick}
+      style={{ ...additionalStyle, fontWeight: FONT_WEIGHT(isBold) }}
+    >
+      {text}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  text: PropTypes.string,
+  isBold: PropTypes.bool,
+  additionalStyle: PropTypes.object
+};
+
+Button.defaultProps = {
+  onClick: defaultOnClickFn,
+  text: 'button',
+  isBold: false,
+  additionalStyle: {}
+};
+
+export default Button;
